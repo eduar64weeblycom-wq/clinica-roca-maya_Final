46e625a8722447c4a50ede94b6ab6ce5024dd2be
Roca Maya <rocamaya631@gmail.com>
2026-08-05 21:42:21 -0600

    Corregir archivo db.js para usar solo PostgreSQL

--- database/db.js
+++ database/db.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -6,5 +7,9 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+
+pool.connect()
+  .then(() => console.log("Base de datos PostgreSQL (Supabase) conectada exitosamente"))
+  .catch(err => console.error("Error de conexión a la base de datos:", err.message));
 
 module.exports = pool;
